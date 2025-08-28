a95f26214f36b7c5a67beb66add94c0e7af6b263
Zeinab AlHussieni <zei.alhussieni@gmail.com>
2025-08-29 00:07:24 +0300

    feat(frontend):Added the first section of homepage

--- dreamnest_client/src/Components/homePage/homeHeader/HomeHeader.tsx
+++ dreamnest_client/src/Components/homePage/homeHeader/HomeHeader.tsx
@@ -6,46 +6,36 @@ import "./homeHeader.css";
 const HomeHeader = () => {
   return (
     <>
-    <section className="welcome-section">
-      <div className="left-section">
-        <h1 className="welcome-title">
-          Your goals are more than just dreams<br />
-          They’re waiting to be unlocked
-        </h1>
+   
+      <section className="section section-text-left">
+        <div className="text-container">
+          <h1 className="title">Your goals are more than just dreams<br />They’re waiting to be unlocked</h1>
+          <p className="description">
+            Inside DreamNest, you’ll discover the path, the people, 
+            and the power to make them real
+          </p>
+        </div>
+        <div className="image-container image-right-gap">
+          <img src={homeImg} alt="Welcome" />
+        </div>
+      </section>
 
-        <p className="welcome-description">
-          Inside DreamNest, you’ll discover the path, the people, 
-          and the power to make them real
-        </p>
-      </div>
-
-      <div className="right-section ">
-        <img src={homeImg} alt="Welcome" />
-      </div>
-
-    </section>
-     <section className="welcome-section">
-      <div className="left-section">
-        <img src={homeImg2} alt="Welcome" />
-      </div>
-
-      
-
-      <div className="right-section2">
-         <h1 className="welcome-title2">
-          Why DreamNest?
-        </h1>
-
-        <p className="welcome-description2">
-          Goals can be overwhelming, but your journey doesn’t have to be
-          DreamNest guides you, connects you with the right people, and turns progress into a rewarding experience.
-        </p>
-        
-      </div>
-    </section>
+   
+      <section className="section section-text-right">
+        <div className="image-container image-left-big">
+          <img src={homeImg2} alt="Welcome" />
+        </div>
+        <div className="text-container text-fullwidth">
+          <h1 className="title">Why DreamNest?</h1>
+          <p className="description">
+            Goals can be overwhelming, but your journey doesn’t have to be. 
+            DreamNest guides you, connects you with the right people, 
+            and turns progress into a rewarding experience.
+          </p>
+        </div>
+      </section>
     </>
   );
-  
 };
 
 export default HomeHeader;
